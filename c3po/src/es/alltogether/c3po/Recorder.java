3c27b59ca67c88c2e71dd789e0ef7abb74dcd55d
Edu <Edu@Edu-PC>
2011-04-01 21:26:02 +0200

    Renamed main activity class

--- c3po/src/es/alltogether/c3po/Recorder.java
+++ c3po/src/es/alltogether/c3po/Recorder.java
@@ -3,7 +3,7 @@ package es.alltogether.c3po;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class Grabadora extends Activity {
+public class Recorder extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
